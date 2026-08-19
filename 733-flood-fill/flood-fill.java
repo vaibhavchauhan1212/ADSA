597bb7864ec0ab0443e7943b3593a0bfbbd99e98
Vaibhav Chauhan <vaibhavchauhan1212@gmail.com>
2026-08-19 15:01:10 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (95.52%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -5,13 +5,13 @@ class Solution {
         DFS(image, sr, sc, originalcolor, color);
         return image;
     }
-    void DFS(int[][] image, int row, int col, int originalcolor, int newcolor){
-        if(row<0||row>=image.length || col<0||col>=image[0].length) return;
-        if (image[row][col] != originalcolor) return;
-        image[row][col] = newcolor;
-        DFS(image, row-1, col, originalcolor, newcolor);
-        DFS(image, row+1, col, originalcolor, newcolor);
-        DFS(image, row, col-1, originalcolor, newcolor);
-        DFS(image, row, col+1, originalcolor, newcolor);
+    public void DFS(int[][] image, int row, int col, int originalcolor, int color){
+        if(row<0 || row>=image.length || col<0 || col>=image[0].length) return;
+        if(image[row][col] != originalcolor) return;
+        image[row][col] = color;
+        DFS(image, row-1, col, originalcolor, color);
+        DFS(image, row+1, col, originalcolor, color);
+        DFS(image, row, col-1, originalcolor, color);
+        DFS(image, row, col+1, originalcolor, color);
     }
 }
